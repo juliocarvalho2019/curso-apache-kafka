88ba5598c1e2a9727d60b581b579ecef85553500
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-08-25 19:24:12 -0300

    add json consumer

--- payment-service/src/main/java/com/julio/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/julio/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,15 +2,29 @@ package com.julio.paymentservice.service.impl;
 
 import com.julio.paymentservice.model.Payment;
 import com.julio.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment);
+
+        log.info("Recebi o pagamento {}", payment);
+        Thread.sleep(1000);
+
+        log.info("Enviando pagamento...");
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
