52d257021b5da04466146541001a973b5ca2fa34
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-08-25 10:31:51 -0300

    send first message

---
+++ str-producer/src/main/java/com/julio/strproducer/services/StringProducerService.java
@@ -0,0 +1,17 @@
+package com.julio.strproducer.services;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class StringProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send("str-topic", message);
+    }
+}
