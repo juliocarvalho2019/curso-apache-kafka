da4f691dfd8234a6b21b4cfd1eaeabaca288a247
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-08-25 13:46:46 -0300

    str consumer created

---
+++ str-consumer/src/test/java/com/julio/strproducer/StrConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julio.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
